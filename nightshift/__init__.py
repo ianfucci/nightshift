c4e39a7bae4ef61e1bd9d417ae51aa70906b787e
Ian Fucci <fucci.ian@gmail.com>
2021-07-06 11:54:01 -0400

    Add imports to __init__.py for use in interactive mode

--- nightshift/__init__.py
+++ nightshift/__init__.py
@@ -1,1 +1,3 @@
-
+from nightshift.bmrb import get_bmrb_url, get_shifts, instant_search, open_website
+from nightshift.plotting import plot2D, plot3D
+from nightshift.selector import Selector, AmideSelector, MethylSelector, AdvancedSelector
